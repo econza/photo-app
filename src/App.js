08d40e78ca7b3a1493cc12960520afdbf6272db8
Vyacheslav Zayats <vz@Vyacheslavs-MacBook-Pro.local>
2021-04-26 18:09:39 +0300

    start project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div>
+      <header>test app</header>
+      <div>картинки</div>
+      <footer>© 2018-2019</footer>
+    </div>
+  );
+}
+
+export default App;
