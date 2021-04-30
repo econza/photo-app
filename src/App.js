339b75baf46f35e61fccfe37ed8ecb375f3384bb
Vyacheslav Zayats <vz@Vyacheslavs-MacBook-Pro.local>
2021-04-30 17:16:31 +0300

    connect images

--- src/App.js
+++ src/App.js
@@ -8,38 +8,34 @@ import { useDispatch, useSelector } from 'react-redux';
 
 
 const App = () => {
-	const images = useSelector(state => state);
-console.log("RFHNY", images)
-	const dispatch = useDispatch();
+  const images = useSelector(state => state.imagesState.images);
 
-	useEffect(() => {
-		fetch('https://boiling-refuge-66454.herokuapp.com/images')
-			.then((res) => res.json())
-			.then((resData) => dispatch(getImages(resData.images)))
-	}, []);
-  
+  console.log("RFHNY", images)
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetch('https://boiling-refuge-66454.herokuapp.com/images')
+      .then((res) => res.json())
+      .then((resData) => dispatch(getImages(resData)))
+  }, []);
+
   return (
     <div className="mainWrapper">
       <header>test app</header>
 
-      {/* {images.map((url) => { */}
-					{/* return ( */}
-          
-          <div className="imgWrapper">
-            <img src={home} />
-          </div>)
+      <div className="imgWrapper">
+        {images.map((images, url) => {
+          return (
+            console.log("URL", url),
+            <div className="imgWrapper">
+              <img src={images.url} />
+            </div>)
+        })}
+      </div>
 
-          
-				{/* })} */}
+      <div className="voidContainer"></div>
 
-      {/* <div className="imgWrapper">
-        <img src={home} />
-        <img src={home} />
-        <img src={home} />
-        <img src={home} />
-        <img src={home} />
-        <img src={home} />
-      </div> */}
       <div className="footerContainer">
         <footer>© 2018-2019</footer>
       </div>
