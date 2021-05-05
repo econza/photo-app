87e4d2af70d708a23a66ba908923831818297882
Vyacheslav Zayats <vz@Vyacheslavs-MacBook-Pro.local>
2021-05-05 14:13:39 +0300

    fix naming

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import Header from './components/header/Header.js';
+import Header from './components/header/header.js';
 import Footer from './components/footer/Footer';
 import VoidContainer from './components/mainContent/VoidContainer';
 import MainContent from './components/mainContent/MainContent';
